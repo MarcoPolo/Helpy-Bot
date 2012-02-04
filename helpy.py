621167cf595e533cd7a7f654ecbc9e4244c1d85e
Marco Munizaga <marco.munizaga@grooveshark.com>
2012-02-04 15:34:02 -0500

    isup method added

--- helpy.py
+++ helpy.py
@@ -3,66 +3,82 @@ import tweepy
 from tweepy.streaming import StreamListener, Stream
 
 class HelpyBot(StreamListener):
-	def __init__(self):
-		self.commands = ['insult', 'compliment']
-		super(HelpyBot, self).__init__()
+    def __init__(self):
+        self.commands = ['insult', 'compliment']
+        super(HelpyBot, self).__init__()
 
-	def on_error(self, status_code):
-		print '[Helpy] Error - status code = %s' % status_code
-		return True
+    def on_error(self, status_code):
+        print '[Helpy] Error - status code = %s' % status_code
+        return True
 
-	def on_timeout(self):
-		return True
+    def on_timeout(self):
+        return True
 
-	# Parses the given status, and routes it to a command.
-	def on_status(self, status):
-		tweet = self.parse_status(status)#status.text)
+    # Parses the given status, and routes it to a command.
+    def on_status(self, status):
+        tweet = self.parse_status(status)#status.text)
 
-		if (tweet['target'] != '@helpy_bot'):
-			print '[Helpy] Tweet not meant for Helpy Bot.'
-			return
+        if (tweet['target'] != '@helpy_bot'):
+            print '[Helpy] Tweet not meant for Helpy Bot.'
+            return
 
-		if (tweet['command'] in self.commands):
-			getattr(self, tweet['command'])(tweet)
-		else:
-			print '[Helpy] Error - unknown command %s' % tweet['command']
+        if (tweet['command'] in self.commands):
+            getattr(self, tweet['command'])(tweet)
+        else:
+            print '[Helpy] Error - unknown command %s' % tweet['command']
 
-	# Tokenizes the text status.
-	def parse_status(self, text):
-		tokens = text.lower().split()
-		parsed = {}
-		parsed['target'] = tokens[0]
-		parsed['command'] = tokens[1]
-		parsed['text'] = tokens[2:]
-		parsed['raw_text'] = ' '.join(tokens[2:])
-		return parsed
+    # Tokenizes the text status.
+    def parse_status(self, text, status):
+        tokens = text.lower().split()
+        parsed = {}
+        parsed['target'] = tokens[0]
+        parsed['command'] = tokens[1]
+        parsed['text'] = tokens[2:]
+        parsed['raw_text'] = ' '.join(tokens[2:])
+        #parsed['user'] = status.user.screen_name
+        return parsed
 
-	# Post text as a tweet to Helpy's account. 
-	def post_tweet(self, text):
-		print text
-			
-	def insult(self, tweet):
-		print "BUTT"
-	
-	def compliment(self, tweet):
-		print "HOWDY"
+    # Post text as a tweet to Helpy's account. 
+    def post_tweet(self, text):
+        print text
+            
+    def insult(self, tweet):
+        print "BUTT"
+    
+    def compliment(self, tweet):
+        print "HOWDY"
 
-	def 
+
+    def isupme(self, tweet):
+        url = tweet['text'][0]
+        user = "dr_choc"
+        #user = tweet['user']
+
+        try:
+            returnCode = urllib.urlopen(url).getcode()
+            if returnCode == 200:
+                response = "Seems to be up from here"
+
+        except:
+            response = "Seems to be down"
+
+        print '@'+user+' '+response
+
 
 if __name__ == '__main__':
 
-	# Setup API credentials.
-	from secretStuff import *
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-	api = tweepy.API(auth)
+    # Setup API credentials.
+    from secretStuff import *
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
 
-	# Setup Helpy to listen to twitter stream.
-	helpy = HelpyBot()
-	helpy.on_status('@Helpy_bot insult @thompson if you would be so kind.')
-	helpy.on_status('insult @thompson if you would be so kind.')
+    # Setup Helpy to listen to twitter stream.
+    helpy = HelpyBot()
+    helpy.on_status('@Helpy_bot insult @thompson if you would be so kind.')
+    helpy.on_status('insult @thompson if you would be so kind.')
 
-	#listener = HelpyBot()
-	#stream = Stream(auth, listener)
-	#stream.filter(follow=(483102366,),)
+    #listener = HelpyBot()
+    #stream = Stream(auth, listener)
+    #stream.filter(follow=(483102366,),)
 
